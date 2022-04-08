65d7469caab84b34b02c461d420aeb9da70a8071
BautistaMalu <89524797+BautistaMalu@users.noreply.github.com>
2022-04-08 11:31:17 -0300

    All done
    Both ejs done

---
+++ Assets/Simuprueba1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simuprueba1 : MonoBehaviour
+{
+    //1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+    public int num1;
+    public int num2;
+    void Start()
+    {
+        if ( num1 == num2)
+        {
+            Debug.Log("Son iguales");
+        }
+        else
+        {
+            Debug.Log("No son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Simuprueba2.cs
+++ Assets/Simuprueba2.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class Simuprueba2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos 
+    //   y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible
+    //   .Luego indicar cuánto dinero sobra o falta.
+    public float num;
+         public float num1;
+          public float num2;
+          float total;
+    public float totaldisponible;
     void Start()
     {
-        
+        total = num1 + num + num2;
+        if (total > totaldisponible)
+        {
+            Debug.Log("Te sobran " + (total - totaldisponible) + "$");
+        }
+       else if (totaldisponible > total)
+        {
+            Debug.Log("Te faltan " + (totaldisponible - total) + "$");
+        }
+        else if (totaldisponible == total)
+        {
+            Debug.Log("Estas justo");
+        }
     }
 
     // Update is called once per frame
